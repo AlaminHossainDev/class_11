75d257240b7946c7c4407f683baa0b01ae9f34c5
procoderalamin <prodeveloperalamin@gmail.com>
2021-12-24 21:31:56 +0600

    upload

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+
+// const heading = document.getElementById('love');
+// heading.innerText = 'Mern-Stack is a Good';
+// const mern = document.getElementsByClassName('ami');
+// const tag = document.getElementsByTagName('a');
+
+// tag[0].innerText = 'click for Book';
+// tag[1].innerText = 'go youtube'
+
+
+const newPara = document.querySelectorAll('.ami');
+console.log(newPara);
